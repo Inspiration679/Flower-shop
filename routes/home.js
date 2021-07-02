d8361cc77a271c0f88c89635657e2dddd0466b6a
Panhead32 <54202786+Panhead32@users.noreply.github.com>
2021-07-02 15:26:53 +0200

    second commit

--- routes/home.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express')
-const router = Router()
-const path = require('path')
-
-router.get('/', (req, res) => {
-    res.render(path.join(__dirname, 'views', 'index.html'))
-})
-
-module.exports = router
