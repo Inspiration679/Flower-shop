489b671a778a0719b185adb8f152fe575199b8df
Panhead32 <54202786+Panhead32@users.noreply.github.com>
2021-07-03 23:28:06 +0200

    adding foot

--- routes/support.js
+++ routes/support.js
@@ -1,4 +1,10 @@
 const {Router} = require('express')
 const router = Router()
 
+router.get('/', (req, res) => {
+    res.render('support', {
+        title: 'Поддержка'
+    })
+})
+
 module.exports = router
