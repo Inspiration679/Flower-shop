681cbe39af965f16cbc4b9b3b4276322fb7d8a91
Panhead32 <54202786+Panhead32@users.noreply.github.com>
2021-07-06 11:33:07 +0200

    w
    gg

--- routes/flowers.js
+++ routes/flowers.js
@@ -1,10 +1,31 @@
 const {Router} = require('express')
+const Flower = require('../models/Flower')
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.render('flowers', {
-        title: 'Цветы'
-    })
+router.get('/', async (req, res) => {
+    try{
+        const flower = await Flower.create({
+            flower_name: 'Fikus',
+            cost: 200,
+            _description: 'good plant',
+            image: 'www'
+        })
+        const temp = await Flower.findAll({
+            attributes: ['flower_name', 'cost']
+        })
+        console.log(temp)
+        res.render('flowers', {
+            title: 'Цветы',
+
+        })
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 })
+
+
 
 module.exports = router
