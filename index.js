1e3815eec22f07e1a0d8297c6f022ef6ff798ec8
Panhead32 <54202786+Panhead32@users.noreply.github.com>
2021-07-06 21:23:07 +0200

    adding image func is working!!!!

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.set('views', 'views')
 
 const storageConfig = multer.diskStorage({
     destination: (req, file, cb) =>{
-        cb(null, "/public/uploads");
+        cb(null, "./public/uploads");
     },
     filename: (req, file, cb) =>{
         cb(null, file.originalname);
@@ -33,10 +33,10 @@ const storageConfig = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-
     if(file.mimetype === "image/png" ||
         file.mimetype === "image/jpg"||
-        file.mimetype === "image/jpeg"){
+        file.mimetype === "image/jpeg" ||
+        file.mimetype === "image/webp"){
         cb(null, true);
     }
     else{
@@ -46,7 +46,7 @@ const fileFilter = (req, file, cb) => {
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
-app.use(multer({storage:storageConfig, fileFilter: fileFilter}).single("image"));
+app.use(multer({storage:storageConfig, fileFilter: fileFilter}).single("flower_image"));
 
 const PORT = process.env.PORT || 3000
 
@@ -57,18 +57,6 @@ app.use('/contacts', contactsRoutes)
 app.use('/cart', cartRoutes)
 app.use('/support', supportRoutes)
 app.use('/create', createRoutes)
-
-app.post('/create-add', (req, res) => {
-    // if(!req.body) return res.sendStatus(400);
-
-    const flower_name = req.body.flower_name
-    const cost = req.body.cost
-    const description = req.body.description
-    const image = req.file.path
-    console.log(image)
-    res.redirect('/create')
-
-})
 
 async function start(){
     try {
