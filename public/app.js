350125735c63c23a93559f8da644a8bb90923720
Panhead32 <54202786+Panhead32@users.noreply.github.com>
2021-07-09 21:28:16 +0200

    reg changes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,10 @@
-
-async function addToCart(id) {
-
-}
+$(function () {
+    $(".btn").click(function () {
+        $(".regist-active").toggleClass("registr-inactive");
+        $(".login-inactive").toggleClass("login-active");
+        $(".signup-inactive").toggleClass("signup-active");
+        $(".signin-active").toggleClass("signin-inactive");
+        $(".reg-button-div-active").toggleClass("reg-button-div-inactive");
+        $(".log-button-div-inactive").toggleClass("log-button-div-active");
+    });
+});
