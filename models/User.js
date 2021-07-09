461444b42ff9dc1e17851dc85922caefe727cab6
Panhead32 <54202786+Panhead32@users.noreply.github.com>
2021-07-09 17:16:03 +0200

    users' database added

--- models/User.js
+++ models/User.js
@@ -1,9 +1,58 @@
-class User {
-    constructor(name, surname, email){
-        this.name = name.toString()
-        this.surname = surname.toString()
-        this.email = email.toString()
+const Sequelize = require('sequelize')
+const sequelize = require('../db_connect/db')
+
+const user = sequelize.define('users', {
+    iduser: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER
+    },
+    user_first_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    user_second_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
+})
 
+const cart = sequelize.define('cart', {
+    id_cart: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER
+    },
+    id_product: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    count: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+})
 
-}
+const password = sequelize.define('passwords', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: Sequelize.INTEGER
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+
+user.hasMany(cart, {onDelete: "cascade"})
+user.hasMany(password, {onDelete: "cascade"})
+
+module.exports = user
